7cba7a008e8023d6f1a22a671bf7c9c2ae9fa4a7
jrios741 <128609722+jrios741@users.noreply.github.com>
2023-03-27 12:31:56 -0400

    Initial commit
    Created some files as a test. Deleting and recreating.

---
+++ 1.cpp
@@ -0,0 +1,8 @@
+//
+//  1.cpp
+//  
+//
+//  Created by seymour on 3/27/23.
+//
+
+#include "1.hpp"
---
+++ 1.hpp
@@ -0,0 +1,13 @@
+//
+//  1.hpp
+//  
+//
+//  Created by seymour on 3/27/23.
+//
+
+#ifndef __hpp
+#define __hpp
+
+#include <stdio.h>
+
+#endif /* __hpp */
